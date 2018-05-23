dbb7f53c56da98c7284e05ad4b8fd73042635432
pdelfan <pdelfan@gmail.com>
2018-05-22 19:29:17 -0700

    tableview setup

--- Emoji Dictionary/Emoji Dictionary/EmojiTableViewController.swift
+++ Emoji Dictionary/Emoji Dictionary/EmojiTableViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class EmojiTableViewController: UITableViewController {
 
+    var emojis: [Emoji] = [
+    Emoji(symbol: "😀", name: "Grinning Face", description: "A typical smiley face.", usage: "happiness"),
+    Emoji(symbol: "😕", name: "Confused Face", description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"),
+    Emoji(symbol: "😍", name: "Heart Eyes", description: "A smiley face with hearts for eoyes.", usage: "love of something; attractive"),
+    Emoji(symbol: "👮", name: "Police Officer", description: "A police officer wearing a blue cap with a gold badge.", usage: "person of authority"),
+    Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle.", usage: "something slow"),
+    Emoji(symbol: "🐘", name: "Elephant", description: "A gray elephant.", usage: "good memory"),
+    Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of spaghetti.", usage: "spaghetti"),
+    Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"),
+    Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"),
+    Emoji(symbol: "📚", name: "Stack of Books", description: "Three coloured books stacked on each other.", usage: "homework, studying"),
+    Emoji(symbol: "💔", name: "Broken Heart", description: "A red, broken heart.", usage: "extreme sadness"),
+    Emoji(symbol: "💤", name: "Snore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"),
+    Emoji(symbol: "🏁", name: "Checkered Flag", description: "A black-and-white checkered flag.", usage: "completion")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -28,24 +44,33 @@ class EmojiTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        if section == 0 {
+            return emojis.count
+        } else {
+            return 0
+        }
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        // Fetch the correct cell type by dequeueing a cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath)
+        
+        // Fetch the model object to be displayed
+        let emoji = emojis[indexPath.row]
+        
+        // Configure the cell's properties with the model objct properties
+        cell.textLabel?.text = "\(emoji.symbol) - \(emoji.name)"
+        cell.detailTextLabel?.text = emoji.description
+        
+        // Return fully configured cell
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
